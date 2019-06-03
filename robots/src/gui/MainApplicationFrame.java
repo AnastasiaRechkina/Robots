833cd1e19d9ff9b601849916a16bb6fa72efffe2
AnastasiaRechkina <51262869+AnastasiaRechkina@users.noreply.github.com>
2019-06-03 21:23:20 +0500

    Update MainApplicationFrame.java

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -1,8 +1,22 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyVetoException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -10,48 +24,136 @@ import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import log.LogEntry;
+import log.LogLevel;
 import log.Logger;
 
-/**
- * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
- * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
- */
-public class MainApplicationFrame extends JFrame
-{
-    private final JDesktopPane desktopPane = new JDesktopPane();
+public class MainApplicationFrame extends JFrame{
+    private JDesktopPane desktopPane = new JDesktopPane();
     
     public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
         int inset = 50;        
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
-
+        setBounds(inset, inset,screenSize.width  - inset*2,screenSize.height - inset*2);
         setContentPane(desktopPane);
+        setJMenuBar(generateMenuBar());  
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        
-        LogWindow logWindow = createLogWindow();
-        addWindow(logWindow);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                super.windowClosing(windowEvent);
+                String ObjButtons[] = {"Да", "Нет"};
+                int PromptResult = JOptionPane.showOptionDialog(null,
+                        "Вы уверены?",
+                        null,
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.WARNING_MESSAGE,
+                        null,
+                        ObjButtons,
+                        ObjButtons[1]);
+                if (PromptResult == JOptionPane.YES_OPTION)
+                    onClosing();
+            }
 
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
+            @Override
+            public void windowOpened(WindowEvent windowEvent) {
+                super.windowOpened(windowEvent);
+                try {
+                    onOpen();
+                } catch (PropertyVetoException e) {
+                    System.out.println("Error with privileges");
+                }
+            }
+        });
+    }  
+    public void setState(FrameState state) throws PropertyVetoException {
+        String description = "public class gui.";
+        for (FrameState.InternalFrameState istate : state.getAllStates()) {
+            if (istate.getTag().equals(description + "GameWindow")) {
+                GameWindow game = new GameWindow(0, 0, this);
+                addWindow(game);
+                game.setState((FrameState.InternalFrameState<GameWindow>) istate);
+            } else if (istate.getTag().equals(description + "LogWindow")) {
+                LogWindow log = createNewLogWindow();
+                addWindow(log);
+                log.setState((FrameState.InternalFrameState<LogWindow>) istate);
+            }
+        }
+    }
 
-        setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    private void onOpen() throws PropertyVetoException {
+        ObjectInputStream ois = getStateFile();
+        FrameState state = null;
+        if (ois != null)
+            try {
+                state = (FrameState) ois.readObject();
+            } catch (IOException e) {
+                System.out.println("Load defaults");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Load defaults");
+            }
+        if (state != null)
+            setState(state);
     }
     
-    protected LogWindow createLogWindow()
+    public static ObjectInputStream getStateFile() {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("save.state"));
+        } catch (IOException ex) {
+            return null;
+        }
+        return ois;
+    }
+    
+    private void onClosing() {
+        ObjectOutputStream oos = createStateStream();
+        if (oos != null) {
+            try {
+                FrameState state = new FrameState(this);
+                oos.writeObject(state);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Can't serialize object, not serializable error");
+            }
+            try {
+                oos.flush();
+                oos.close();
+            } catch (IOException ex) {
+                System.out.println("Can't flush and close");
+            }
+        }
+        System.exit(0);
+    }
+    
+    private ObjectOutputStream createStateStream() {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("save.state"));
+        } catch (IOException ex) {
+            File stFile = new File("save.state");
+            try {
+                stFile.createNewFile();
+                oos = new ObjectOutputStream(new FileOutputStream("save.state"));
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return oos;
+    }
+            
+    protected LogWindow createNewLogWindow()
     {
-        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+    	Logger.getDefaultLogSource().releaseListeners();
+        Logger.clear();
+    	LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
         logWindow.setLocation(10,10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
@@ -60,84 +162,59 @@ public class MainApplicationFrame extends JFrame
         return logWindow;
     }
     
+    public JInternalFrame[] getAllFrames() {
+        return desktopPane.getAllFrames();
+    }
+    
     protected void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
     
-//    protected JMenuBar createMenuBar() {
-//        JMenuBar menuBar = new JMenuBar();
-// 
-//        //Set up the lone menu.
-//        JMenu menu = new JMenu("Document");
-//        menu.setMnemonic(KeyEvent.VK_D);
-//        menuBar.add(menu);
-// 
-//        //Set up the first menu item.
-//        JMenuItem menuItem = new JMenuItem("New");
-//        menuItem.setMnemonic(KeyEvent.VK_N);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("new");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        //Set up the second menu item.
-//        menuItem = new JMenuItem("Quit");
-//        menuItem.setMnemonic(KeyEvent.VK_Q);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("quit");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        return menuBar;
-//    }
-    
-    private JMenuBar generateMenuBar()
-    {
+    private JMenuBar generateMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-        
-        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
-        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-        
-        {
-            JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-            systemLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(systemLookAndFeel);
-        }
-
-        {
-            JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-            crossplatformLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-        }
-
-        JMenu testMenu = new JMenu("Тесты");
-        testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Тестовые команды");
-        
-        {
-            JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-            addLogMessageItem.addActionListener((event) -> {
-                Logger.debug("Новая строка");
-            });
-            testMenu.add(addLogMessageItem);
-        }
-
-        menuBar.add(lookAndFeelMenu);
-        menuBar.add(testMenu);
+        menuBar.add(documentItem());
         return menuBar;
+    }
+
+    private JMenu documentItem() {
+        JMenu menu = new JMenu("Игра");
+        menu.setMnemonic(KeyEvent.VK_D);
+        menu.add(newDoc());
+        menu.add(newGame());
+        menu.add(quitApp());
+        return menu;
+    }
+
+    private JMenuItem newDoc() {
+        JMenuItem menuItem = new JMenuItem("Логи");
+        menuItem.setMnemonic(KeyEvent.VK_L);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.ALT_MASK));
+        menuItem.addActionListener(actionEvent -> addWindow(createNewLogWindow()));
+        return menuItem;
+    }
+
+    private JMenuItem newGame() {
+        JMenuItem menuItem = new JMenuItem("Новая игра");
+        menuItem.setMnemonic(KeyEvent.VK_G);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.ALT_MASK));
+        menuItem.addActionListener(actionEvent -> addWindow(new GameWindow(400, 400, this)));
+        return menuItem;
+    }
+
+    private JMenuItem quitApp() {
+        JMenuItem menuItem = new JMenuItem("Выход");
+        menuItem.setMnemonic(KeyEvent.VK_Q);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+
+        menuItem.setActionCommand("Выход");
+        menuItem.addActionListener(actionEvent ->
+                Toolkit.getDefaultToolkit()
+                        .getSystemEventQueue()
+                        .postEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING)));
+        return menuItem;
     }
     
     private void setLookAndFeel(String className)
@@ -149,8 +226,5 @@ public class MainApplicationFrame extends JFrame
         }
         catch (ClassNotFoundException | InstantiationException
             | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            // just ignore
-        }
-    }
-}
+        {}
+    }}
