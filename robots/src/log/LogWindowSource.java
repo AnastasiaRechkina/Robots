ba9031441ffc9a7797b171de4396e235d0d5d1ad
AnastasiaRechkina <51262869+AnastasiaRechkina@users.noreply.github.com>
2019-06-03 21:28:45 +0500

    Update LogWindowSource.java

--- robots/src/log/LogWindowSource.java
+++ robots/src/log/LogWindowSource.java
@@ -2,28 +2,20 @@ package log;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
-/**
- * Что починить:
- * 1. Этот класс порождает утечку ресурсов (связанные слушатели оказываются
- * удерживаемыми в памяти)
- * 2. Этот класс хранит активные сообщения лога, но в такой реализации он 
- * их лишь накапливает. Надо же, чтобы количество сообщений в логе было ограничено 
- * величиной m_iQueueLength (т.е. реально нужна очередь сообщений 
- * ограниченного размера) 
- */
 public class LogWindowSource
 {
     private int m_iQueueLength;
     
-    private ArrayList<LogEntry> m_messages;
+    private LinkedList<LogEntry> m_messages;
     private final ArrayList<LogChangeListener> m_listeners;
     private volatile LogChangeListener[] m_activeListeners;
     
     public LogWindowSource(int iQueueLength) 
     {
-        m_iQueueLength = iQueueLength;
-        m_messages = new ArrayList<LogEntry>(iQueueLength);
+    	m_iQueueLength = iQueueLength;
+        m_messages = new LinkedList<>();
         m_listeners = new ArrayList<LogChangeListener>();
     }
     
@@ -45,26 +37,26 @@ public class LogWindowSource
         }
     }
     
-    public void append(LogLevel logLevel, String strMessage)
-    {
+    public void append(LogLevel logLevel, String strMessage) {
         LogEntry entry = new LogEntry(logLevel, strMessage);
-        m_messages.add(entry);
-        LogChangeListener [] activeListeners = m_activeListeners;
-        if (activeListeners == null)
-        {
-            synchronized (m_listeners)
-            {
+        addMessage(entry);
+        onUpdateListeners();
+    }
+
+    public void onUpdateListeners() {
+        if (m_activeListeners == null)
+            synchronized (m_listeners) {
                 if (m_activeListeners == null)
-                {
-                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
-                    m_activeListeners = activeListeners;
-                }
+                    m_activeListeners = m_listeners.toArray(new LogChangeListener[0]);
             }
-        }
-        for (LogChangeListener listener : activeListeners)
-        {
+        for (LogChangeListener listener : m_activeListeners)
             listener.onLogChanged();
-        }
+    }
+    
+    public void addMessage(LogEntry entry) {
+        if (size() >= m_iQueueLength)
+            m_messages.remove();
+        m_messages.add(entry);
     }
     
     public int size()
@@ -72,18 +64,16 @@ public class LogWindowSource
         return m_messages.size();
     }
 
-    public Iterable<LogEntry> range(int startFrom, int count)
-    {
-        if (startFrom < 0 || startFrom >= m_messages.size())
-        {
-            return Collections.emptyList();
-        }
-        int indexTo = Math.min(startFrom + count, m_messages.size());
-        return m_messages.subList(startFrom, indexTo);
-    }
-
     public Iterable<LogEntry> all()
     {
         return m_messages;
+    }
+
+    public void releaseListeners() {
+        m_listeners.clear();
+    }
+
+    public void clear() {
+        m_messages.clear();
     }
 }
